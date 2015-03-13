7d90634cf28012b1b6d5c6d6a791632a651272f2
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2015-03-13 14:37:26 +0100

    Adapted interval

--- app/models/interval.rb
+++ app/models/interval.rb
@@ -5,20 +5,12 @@ class Interval
   field :from, :type => Integer
   field :to, :type => Integer
 
-  def from
-    return :from
-  end
-
-  def to
-    return :to
-  end
-
   def duration
-    return (:to - :from)
+    return (to - from)
   end
 
   def isInfinite
-    #TODO: what's the convention for infnity?
+    to.nil? && !from.nil?
   end
 
 end
