c0142a46884e5a168a1aa57d7d1897fb6c255f75
Aliaksei Syrel <alex.syrel@gmail.com>
2015-03-13 16:33:34 +0100

    Fixed document root error

---
+++ app/controllers/infos_controller.rb
@@ -0,0 +1,7 @@
+class InfosController < ApplicationController
+
+  def introduction
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
 
-  resources :mdcs, :except => [:new, :edit, :update, :destroy, :create] do
-    get :name, :on => :collection
-    get :drgs, :on => :member
-  end
-
-  resources :chop_codes, :except => [:new, :edit, :update, :destroy, :create] do
-    get :search, :on => :collection
-    get :name, :on => :collection
-  end
-
-  resources :icd_codes, :except => [:new, :edit, :update, :destroy, :create] do
-    get :search, :on => :collection
-    get :name, :on => :collection
-  end
+  # resources :mdcs, :except => [:new, :edit, :update, :destroy, :create] do
+  #   get :name, :on => :collection
+  #   get :drgs, :on => :member
+  # end
+  #
+  # resources :chop_codes, :except => [:new, :edit, :update, :destroy, :create] do
+  #   get :search, :on => :collection
+  #   get :name, :on => :collection
+  # end
+  #
+  # resources :icd_codes, :except => [:new, :edit, :update, :destroy, :create] do
+  #   get :search, :on => :collection
+  #   get :name, :on => :collection
+  # end
 
   namespace :api do
     namespace :v1 do
@@ -23,16 +23,16 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :drgs, :except => [:new, :edit, :update, :destroy, :create] do
-    get :name, :on => :collection
-    get :search, :on => :collection
-  end
+  # resources :drgs, :except => [:new, :edit, :update, :destroy, :create] do
+  #   get :name, :on => :collection
+  #   get :search, :on => :collection
+  # end
 
-  get 'contact', :to => 'infos#contact'
-  
+  # get 'contact', :to => 'infos#contact'
+  #
   root :to => 'infos#introduction'
-  
+  #
   get 'set_locale', :to => 'application#set_locale'
-  get 'search', :to => 'search#index'
+  # get 'search', :to => 'search#index'
   # See how all your routes lay out with "rake routes"
 end
