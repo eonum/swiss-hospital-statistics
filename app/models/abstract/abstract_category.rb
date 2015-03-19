071e2430bd17d21893f440ac960bfbb264440e25
Tomasz <tz.kolonko@gmail.com>
2015-03-19 20:51:00 +0100

    Added some Documentation

--- app/models/abstract/abstract_category.rb
+++ app/models/abstract/abstract_category.rb
@@ -4,12 +4,19 @@ class AbstractCategory
 
   field :categories, :type => Hash, :default => { }
 
+  # tags will be used to identify the category
+  # default value is set to unknown
   @tag = 'unknown'
 
+  # class method that returns the variable @tag
   def self.tag
     @tag
   end
 
+  # Tests if category is already present. If it is
+  # the category is pushed to existing hash key, otherwise
+  # a new key value pair is created.
+  # param category a category to be added ()eg. PercentilCategory)
   def add (category)
     categories_copy = self.categories.deep_copy
     categories_copy[category.class.tag.to_s] = [] unless self.at(category.class.tag)
@@ -17,14 +24,19 @@ class AbstractCategory
     self.categories = categories_copy
   end
 
+  # Adds several categories to the class field.
   def add_all (categories)
     categories.each { |each| self.add(each) }
   end
 
+  # if category_id is already present in categories hash
+  # it will return its value thus true, otherwise false
+  # param category_id it is the tag string of a category class
   def at (category_id)
     self.categories[category_id.to_s]
   end
 
+  # TODO ask Alex about the &block
   def at_find (category_id, &block)
     return self.at(category_id) unless block_given?
     selected = self.at(category_id).select(&block)
