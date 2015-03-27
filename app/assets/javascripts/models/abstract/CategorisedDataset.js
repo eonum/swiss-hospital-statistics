57ce7d85deedd9ad11741a5961b3a7c87e407833
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2015-03-27 17:50:57 +0100

    Renamed file I forgot

---
+++ app/assets/javascripts/models/abstract/CategorisedDataset.js
@@ -0,0 +1,30 @@
+define([
+    'models/abstract/AbstractDataset',
+    'models/abstract/CategorisedData',
+], function(
+    AbstractDataset,
+    CategorisedData
+) {
+
+    function CategorisedDataset () {
+        var _this = new AbstractCode();
+
+        /**
+         * @returns {string}
+         */
+        _this.name = function() {
+            return 'Categorised Dataset';
+        };
+
+        /**
+         * @param {Number} year
+         * @returns {CategorisedData}
+         */
+        _this.newData = function (year) {
+            return new CategorisedData(year);
+        };
+
+        return _this;
+    }
+    return CategorisedDataset;
+});
