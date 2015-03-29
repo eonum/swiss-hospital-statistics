a4fb3efe17b1568e80d9aeff17e09e77714b36bb
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2015-03-29 19:00:44 +0200

    Added key to pie chart

--- app/assets/javascripts/views/ui/PieChartByAgeVisualisation.js
+++ app/assets/javascripts/views/ui/PieChartByAgeVisualisation.js
@@ -41,7 +41,7 @@ define([
                     var to = interval.interval.to;
 
                     var textInterval;
-                    if (interval.to == "") {
+                    if (_.isUndefined(to)) {
                         textInterval = from + "+";
                     } else {
                         textInterval = from + " - " + to;
@@ -63,6 +63,20 @@ define([
 
                 content.add('<h3>' + description + '</h3>');
                 content.add(pieChart);
+
+                // TODO: actually get colors from d3
+                var colors = ["#1f77b4", "#aec7e8", "#ff7f0e",
+                    "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896",
+                    "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2",
+                    "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#17becf", "#9edae5"];
+
+                var key = '<div id="key"><ul>';
+                $.each(intervals, function(index, value){
+                    console.log(index);
+                    key += '<li style="background-color:' + colors[index] + '">' + value.key + '   Jahre</li>';
+                });
+                key += '</ul></div>';
+                content.add(key);
             }
 
             return _this;
