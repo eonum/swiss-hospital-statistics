f9e9214464ea8e0ee56460a5f59d72814fbf7f92
Tomasz <tz.kolonko@gmail.com>
2015-04-04 23:02:11 +0200

    Started BoxPlotting

---
+++ app/assets/javascripts/views/BoxPlot.js
@@ -0,0 +1,92 @@
+define(['d3', 'views/ResponsiveSvg'], function (d3, ResponsiveSvg) {
+
+    /**
+     * @returns {*|jQuery|HTMLElement}
+     * @constructor
+     * @class BoxPlot
+     */
+    function BoxPlot(_width, _height){
+        var _this = new ResponsiveSvg(_width, _height);
+        var radius = Math.min(_this._width(), _this._height()) / 2;
+        var path = _this.svg()
+            .append("g")
+            .attr("transform", "translate(" + _this._width() / 2 + "," + _this._height() / 2 + ")")
+            .selectAll("path");
+
+
+
+
+
+        var utils = {
+            findNeighborArc : function(i, data0, data1, key) {
+                var d;
+                return (d = utils.findPreceding(i, data0, data1, key)) ? {startAngle: d.endAngle, endAngle: d.endAngle}
+                    : (d = utils.findFollowing(i, data0, data1, key)) ? {startAngle: d.startAngle, endAngle: d.startAngle}
+                    : null; },
+            findPreceding : function(i, data0, data1, key) {
+                var m = data0.length;
+                while (--i >= 0) {
+                    var k = key(data1[i]);
+                    for (var j = 0; j < m; ++j) {
+                        if (key(data0[j]) === k) return data0[j]; } } },
+            findFollowing : function(i, data0, data1, key) {
+                var n = data1.length, m = data0.length;
+                while (++i < n) {
+                    var k = key(data1[i]);
+                    for (var j = 0; j < m; ++j) {
+                        if (key(data0[j]) === k) return data0[j]; } } },
+            arcTween : function(d) {
+                var i = d3.interpolate(this._current, d);
+                this._current = i(0);
+                return function(t) { return arc(i(t)); }; }
+        };
+
+        _this._keyOf = function (d) {return d.data[_this._keySymbol()]};
+        _this._valueOf = function (data) {return data.data[_this._valueSymbol()]};
+        _this._colorOf = function(value) { return d3.scale.category20()(value) };
+        _this._labelOf = function(value) {return value.toString()};
+        _this._keySymbol = function() {return 'key'};
+        _this._valueSymbol = function() {return 'value'};
+
+        var arc = d3.svg.arc().innerRadius(radius - 100).outerRadius(radius - 20);
+        _this.initialize = function(data) {
+
+            var pie = d3.layout
+                .pie()
+                .value(function(d) { return d[_this._valueSymbol()]; })
+                .sort(null);
+
+            var data0 = path.data();
+            var data1 = pie(data);
+
+            path = path.data(data1, _this._keyOf);
+
+            path.enter().append("path")
+                .each(function(d, i) { this._current = utils.findNeighborArc(i, data0, data1, _this._keyOf) || d; })
+                .attr("fill", function(d) { return _this._colorOf(_this._valueOf(d), d); })
+                .append("title")
+                .text(_this._keyOf);
+
+            path.exit()
+                .datum(function(d, i) { return utils.findNeighborArc(i, data1, data0, _this._keyOf) || d; })
+                .transition()
+                .duration(750)
+                .attrTween("d", utils.arcTween)
+                .remove();
+
+            path.transition()
+                .duration(750)
+                .attrTween("d", utils.arcTween);
+
+            path.enter().append("text")
+                .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+                .attr("dy", ".35em")
+                .style("text-anchor", "middle")
+                .text(function(d) { return _this._labelOf(_this._valueOf(d), d); });
+        };
+        return _this;
+    }
+
+    return AbstractPieChart;
+
+});
---
+++ app/assets/javascripts/views/ui/BoxPlotVisualisation.js
@@ -0,0 +1,90 @@
+define([
+    'View', 'views/PieChart'
+], function (
+    View, PieChart
+) {
+
+    function PieChartByAgeVisualisation() {
+        var _this = new View('<div></div>');
+        var content = new View('<div></div>');
+        var pieChart;
+
+        _this.initialize = function () {
+            this.append(content);
+            _this.class('pieChartContainer');
+        };
+
+        _this.add = override(_this, _this.add, function(element){
+            content.add(element);
+            return _this;
+        });
+
+        /**
+         * Creates this visualisation from the data provided.
+         * @param data the data to update this visualisation with
+         */
+        _this.setData = function (data){
+            var datasets = data.codes.icd.codes;
+            if(datasets.length > 0) {
+                var code = datasets[0].code;
+                var description = datasets[0].description;
+                var intervals = [];
+
+                // get total number of cases
+                var sum = 0;
+                for (var i = 0; i < datasets.length; i++) {
+                    sum += datasets[i].categorised_data.categories.interval[0].n;
+                }
+
+                for (var i = 0; i < datasets.length; i++) {
+                    var interval = datasets[i].categorised_data.categories.interval[0];
+                    var from = interval.interval.from;
+                    var to = interval.interval.to;
+
+                    var textInterval;
+                    if (_.isUndefined(to)) {
+                        textInterval = from + "+";
+                    } else {
+                        textInterval = from + " - " + to;
+                    }
+
+                    intervals.push({key: textInterval, value: 100 * (interval.n) / sum});
+                }
+
+                pieChart = new PieChart().transformed(function (v) {
+                    return v.toPrecision(3)
+                })
+                    .labeled(function (value) {
+                        return (value ) + '%'
+                    })
+                    .openOn(intervals);
+
+                content.empty();
+
+                content.add('<h3>' + code + ": "+ description + '</h3>');
+                content.add(pieChart);
+
+                // TODO: actually get colors from d3
+                var colors = ["#1f77b4", "#aec7e8", "#ff7f0e",
+                    "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896",
+                    "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2",
+                    "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#17becf", "#9edae5"];
+
+                var key = '<div id="key"><ul>';
+                $.each(intervals, function(index, value){
+                    key += '<li style="background-color:' + colors[index] + '">' + value.key + '   Jahre</li>';
+                });
+                key += '</ul></div>';
+                content.add(key);
+            }
+
+            return _this;
+        };
+
+        _this.initialize();
+
+        return _this;
+    }
+
+    return PieChartByAgeVisualisation;
+});
