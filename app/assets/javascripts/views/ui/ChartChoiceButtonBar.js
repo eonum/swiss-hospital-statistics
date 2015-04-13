747b7a2d9ff235f0678a9bc48b1f5e331d42d57e
Silas Berger <berger.silu@hotmail.com>
2015-04-13 14:36:44 +0200

    Removed obsolete method in ChartChoiceButtonBar

--- app/assets/javascripts/views/ui/ChartChoiceButtonBar.js
+++ app/assets/javascripts/views/ui/ChartChoiceButtonBar.js
@@ -13,20 +13,6 @@ define([
 
         _this.add(content);
 
-        _this.addButtons = function (cardPane) {
-            var barChartButton = $(new View('<a href="#" class="small button secondary">Bar Chart</a>'));
-            barChartButton.click(function(){cardPane.setCard("barChart")});
-            content.add(new View('<li></li>').add(barChartButton));
-
-            var ordinalCurveButton = $(new View('<a href="#" class="small button secondary">Ordinal Curve</a>'));
-            ordinalCurveButton.click(function(){cardPane.setCard("ordinalCurve")});
-            content.add(new View('<li></li>').add(ordinalCurveButton));
-
-            var pieChartButton = $(new View('<a href="#" class="small button secondary">ICD Pie Chart</a>'));
-            pieChartButton.click(function(){cardPane.setCard("pieChart")});
-            content.add(new View('<li></li>').add(pieChartButton));
-        };
-
         _this.addButton = function(cardPane, targetID, title){
             var button = new $(new View('<a href="#" class="small button secondary">' + title +'</a>'));
             button.click(function(){cardPane.setCard(targetID)});
