ce8cc7c9b33c78cb8825a457525b1fbd945d3fdf
Silas Berger <berger.silu@hotmail.com>
2015-05-08 14:14:43 +0200

    Added some chart lables, not yet correctly positioned

--- app/assets/javascripts/views/widgets/top3/TopThreeDiagnosisTable.js
+++ app/assets/javascripts/views/widgets/top3/TopThreeDiagnosisTable.js
@@ -10,21 +10,20 @@ define([
 
     function TopThreeDiagnosisTable(_width, _height){
 
-        //Expected JSON-format: {"interval": val, "total": val, "male": val, "female": val}
-
         var _this = new ResponsiveSvg(_width, _height);
 
         var TRANSITION_TIME = 1000;
         var titleFontSize = _height / 20;
         var chartHeight = _height - titleFontSize;
-        var topOffset = 60; //TODO another magic number...
+        var topOffset = 80; //TODO another magic number...
+        var leftOffset = 100;
 
         var hospitalTypeButtons = new HospitalTypeButtonBar();
 
         _this.append(hospitalTypeButtons);
 
         _this.marginTop(50);
-        _this.marginLeft(50);
+        _this.marginLeft(30);
         _this.marginRight(140);
         _this.marginBottom(50);
 
@@ -37,7 +36,7 @@ define([
                 .attr("y", 35)
                 .style("font-size", titleFontSize + "px");
 
-            _this.setTitle("Three majestic black (actually now red, well, now pink) squares in their natural habitat. Also, there are 9 squares...");
+            _this.setTitle("Nine majestic pink boxes in their natural habitat");
         };
 
         _this.setTitle = function(text){
@@ -67,15 +66,47 @@ define([
                 .data(data)
                 .enter().append("g");
 
+            this.svg().append("text")
+                .attr("x", 100)
+                .attr("y", 70)
+                .text("Total");
+
+            this.svg().append("text")
+                .attr("x", 250)
+                .attr("y", 70)
+                .text("Male");
+
+            this.svg().append("text")
+                .attr("x", 500)
+                .attr("y", 70)
+                .text("Female");
+
+            this.svg().append("text")
+                .attr("x", _this.marginLeft())
+                .attr("y", 70)
+                .text("0-14");
+
+            this.svg().append("text")
+                .attr("x", _this.marginLeft())
+                .attr("y", 130)
+                .text("15-39");
+
+            this.svg().append("text")
+                .attr("x", _this.marginLeft())
+                .attr("y", 250)
+                .text("40-69");
+
+
+
             boxGroup.append("rect")//TODO: loads of magic numbers here...
-                .attr("x", function(d, i) {return xScale(i%3)})
+                .attr("x", function(d, i) {return xScale(i%3) + leftOffset})
                 .attr("y", function(d, i) { return yScale((Math.floor(i/3))) + topOffset})
                 .attr("height", function(d) { return boxHeight - (padding/2) })
                 .attr("width", function(d) { return boxWidth - (padding/2) })
                 .style("fill", function(d) { return "pink" });
 
             boxGroup.append("text")
-                .attr("x", function(d, i) { return xScale(i%3) + (boxWidth/2) - (padding/2)})
+                .attr("x", function(d, i) { return xScale(i%3) + (boxWidth/2) + leftOffset - (padding/2)})
                 .attr("y", function(d, i) { return yScale((Math.floor(i/3))) + (boxHeight/2) + topOffset})
                 .style('font-size', '20px')
                 .attr('class', 'light-font')
