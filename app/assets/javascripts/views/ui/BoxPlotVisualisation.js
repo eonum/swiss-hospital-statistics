cf362a15493c39a388e37b4f4c0f2f95b8b0da7e
Tomasz <tz.kolonko@gmail.com>
2015-05-11 21:04:59 +0200

    minor refactoring

--- app/assets/javascripts/views/ui/BoxPlotVisualisation.js
+++ app/assets/javascripts/views/ui/BoxPlotVisualisation.js
@@ -33,8 +33,6 @@ define([
                 })
                 .legend(function(entity) {
                     var data = entity.data[0];
-                    console.log("entity");
-                    console.log(entity);
                     return [
                         {
                             value: data.higherWhisker,
