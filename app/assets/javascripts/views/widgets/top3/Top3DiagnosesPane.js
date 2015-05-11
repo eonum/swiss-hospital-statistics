499993a58b564d2883212b9ecf713a7aae87a480
Silas Berger <silas.berger@students.unibe.ch>
2015-05-11 14:57:49 +0200

    setup model for hospital type and year buttons

--- app/assets/javascripts/views/widgets/top3/Top3DiagnosesPane.js
+++ app/assets/javascripts/views/widgets/top3/Top3DiagnosesPane.js
@@ -2,30 +2,41 @@ define([
     'View',
     'views/widgets/top3/TopThreeDiagnosisVisualisation',
     'views/widgets/Selector',
-    'models/SelectorModel'
+    'models/SelectorModel',
+    'announcements/OnSelectionChanged'
 ], function(
     View,
     TopThreeDiagnosisVisualisation,
     Selector,
-    SelectorModel
+    SelectorModel,
+    OnSelectionChanged
 ){
 
     function Top3DiagnosesPane() {
         var _this = new View('<div></div>');
-        var model = new SelectorModel(); //TODO: shouldn't be able to select multiple years, can't unselect button
-        var widget = new Selector().model(model);
-
+        var model = new SelectorModel(); //TODO: shouldn't be able to select multiple years
+        var widget = {};
 
         _this.initialize = function () {
-            _this.add(new TopThreeDiagnosisVisualisation(800, 400));
             model.add('years').items([2011, 2012, 2013]);
             model.add('hospitals').items([  'Allgemeine Krankenhäuser, Zentrumsversorgung',
                                             'Allgemeine Krankenhäuser, Grundversorgung',
                                             'Spezialkliniken: Psychiatrische Kliniken',
                                             'Spezialkliniken: Rehabilitationskliniken',
                                             'Spezialkliniken: Andere Spezialkliniken']);
+            model.announcer().onSendTo(OnSelectionChanged, _this.onChanged, _this);
 
+            var widget = new Selector().model(model);
             _this.add(widget);
+            _this.model(model);
+
+            _this.add(new TopThreeDiagnosisVisualisation(800, 400));
+        };
+
+        _this.onChanged = function(ann) {
+            //TODO: implement this
+            //TODO: shouldn't be able to select multiple years
+            console.log(ann.selection());
         };
 
         _this.initialize();
